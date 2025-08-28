515b9498dd0541a77cfb73f406763e3c7147e245
Alexander <52969955+Oxigen-a@users.noreply.github.com>
2025-08-28 13:27:44 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     description: 'Concordia image registry for Kasm supported workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://github.com/ConU-CSSE/kasm-registry/',
-    contactUrl: 'https://github.com/ConU-CSSE/kasm-registry/support',
+    contactUrl: 'https://github.com/ConU-CSSE/kasm-registry/issue',
   },
   reactStrictMode: true,
   basePath: '/kasm-registry/1.1',
